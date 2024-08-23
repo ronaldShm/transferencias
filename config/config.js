bbf0ea9595c725a67a3562c51a7f02093a72607e
Ronald Shumery <ronaldshumery@gmail.com>
2024-08-22 21:13:24 -0400

    añadiendo variables de entorno para probar deploy en railway

--- config/config.js
+++ config/config.js
@@ -1,15 +1,10 @@
 const mysql = require('mysql2');
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
 });
 
 connection.connect((err) => {
